6c31e34afc71c71361b59d57d4d443efacc58514
karlmacas29 <karlracomamacas@gmail.com>
2025-05-26 11:01:52 +0800

    modified:   .env.example 	modified:   config/cors.php

--- config/cors.php
+++ config/cors.php
@@ -16,20 +16,13 @@ return [
     */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
     'allowed_methods' => ['*'],
-
     // Allows requests from any domain
     'allowed_origins' => ['*'],
-
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => true,
 
 ];
